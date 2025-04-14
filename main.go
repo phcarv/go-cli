f7dc9b2fe767987415741591a1962155dbcbef1f
phcarv <p.hcarvalho@outlook.com>
2025-04-14 17:26:01 -0300

    including dotenv read

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 import (
 	"fmt"
-	//"net/url"
+	//"net/url" 
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main(){
+	err := godotenv.Load()
+	if err != nil {
+		panic("Wasn't able to load .env")
+	}
+
 	//var quote = "The sun himself is weak when he first rises, and gathers strength and courage as the day gets on. - Charles Dickens."
 
 	//resp, err := http.Post("http://example.com/upload", "image/jpeg", &buf)
